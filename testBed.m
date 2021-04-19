166f2031b84e8eb6de0292d69b4a13bcf4b61c9a
Nicholas Meyer <nicholas.watney@gmail.com>
2021-04-18 22:55:28 -0400

    let's go, baby

--- testBed.m
+++ testBed.m
@@ -6,7 +6,7 @@ Fs = 100;
 Ts = 1/Fs;
 n = 0:Ts:1-Ts;
 
-SNR = 10;
+SNR = -10;
 
 x_n = sin(2*pi*(1:10)'*n);
 
@@ -17,6 +17,6 @@ figure();
 for i = 1:10
     subplot(5, 2, i);
     hold on;
-    stem(signal(i,:));
+    stem(x_n(i,:));
     stem(noisySignal(i,:));
 end
